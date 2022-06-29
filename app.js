6374488b578b77988fb27c0c0725046920f5feb6
abuSamrah <abdullah@kiitos-tech.com>
2022-06-29 17:10:01 +0300

    view engines

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const express = require("express");
 
 const app = express();
+// register view engine
+app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname }); // it infers the status_code & content_type
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "about", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname }); // it infers the status_code & content_type
+  res.render("about", { title: "about", blogs: [] });
 });
 
 app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+  res.redirect("about");
+});
+
+app.get("/blog/create", (req, res) => {
+  res.render("create", { title: "create", blogs: [] });
 });
 
 // Express handle routes from top to the bottom
 app.use((_, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404", blogs: [] });
 });
